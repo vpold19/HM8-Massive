d6183ee9fe0b7291bee0fad166a57cf1ef053ca7
vadim <vadimpoldolin19@gmail.com>
2023-03-04 16:32:59 +0100

    Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1(){
+        System.out.println("Задача 1");
+
+    }
+
+
+
+}
